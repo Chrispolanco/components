2167b281427d269a3e42039136f7ad020b062c23
Chrispolanco <chrispolanco@sbcglobal.net>
2022-01-04 23:29:44 -0800

    created ApprovalCard component

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -4,10 +4,10 @@ const CommentDetail = (props) => {
     return (
         <div className='comment'>
             <a href="/" className="avatar">
-                <img alt="avatar" src={props.animalImage}/> 
+                <img alt="avatar" src={props.avatar}/> 
             </a> 
             <div className="content">
-                <a href="/" classname="author">
+                <a href="/" className="author">
                     {props.author}
                 </a>
                 <div className="metadata">
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom'; 
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 import faker from 'faker';
 
 const App = () => {
     return (
         <div className='ui container comments'> 
-            <CommentDetail author = "Sam" timeAgo = "Today at 4:35PM" content = "I agree" animalImage = {faker.image.animals()}/> 
-            <CommentDetail author = "Alex" timeAgo = "Today at 5:35PM" content = "I also agree" animalImage = {faker.image.animals()}/> 
-            <CommentDetail author = "Jane" timeAgo = "Today at 6:35PM" content = "I don't agree as much" animalImage = {faker.image.animals()}/> 
+            <CommentDetail 
+                author = "Sam" 
+                timeAgo = "Today at 4:35PM" 
+                content = "I agree" 
+                avatar = {faker.image.nature()}
+            /> 
+            <CommentDetail 
+                author = "Alex" 
+                timeAgo = "Today at 5:35PM" 
+                content = "I also agree" 
+                avatar = {faker.image.city()}
+            /> 
+            <CommentDetail 
+                author = "Jane" 
+                timeAgo = "Today at 6:35PM" 
+                content = "I don't agree as much" 
+                avatar = {faker.image.city()}
+            /> 
         </div>
     ); 
 }; 
+
 
 ReactDOM.render(<App/>, document.getElementById('root')); 
 
