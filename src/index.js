09c30c3ed9bd6eec3fdf02658efaa96378a9e1d4
Chrispolanco <chrispolanco@sbcglobal.net>
2022-01-04 23:43:59 -0800

    wrapped component within component

--- src/index.js
+++ src/index.js
@@ -7,24 +7,32 @@ import faker from 'faker';
 const App = () => {
     return (
         <div className='ui container comments'> 
-            <CommentDetail 
-                author = "Sam" 
-                timeAgo = "Today at 4:35PM" 
-                content = "I agree" 
-                avatar = {faker.image.nature()}
-            /> 
-            <CommentDetail 
-                author = "Alex" 
-                timeAgo = "Today at 5:35PM" 
-                content = "I also agree" 
-                avatar = {faker.image.city()}
-            /> 
-            <CommentDetail 
-                author = "Jane" 
-                timeAgo = "Today at 6:35PM" 
-                content = "I don't agree as much" 
-                avatar = {faker.image.city()}
-            /> 
+            <ApprovalCard>
+                <CommentDetail 
+                    author = "Sam" 
+                    timeAgo = "Today at 4:35PM" 
+                    content = "I agree" 
+                    avatar = {faker.image.nature()}
+                /> 
+            </ApprovalCard> 
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author = "Alex" 
+                    timeAgo = "Today at 5:35PM" 
+                    content = "I also agree" 
+                    avatar = {faker.image.city()}
+                /> 
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author = "Jane" 
+                    timeAgo = "Today at 6:35PM" 
+                    content = "I don't agree as much" 
+                    avatar = {faker.image.city()}
+                /> 
+            </ApprovalCard>
         </div>
     ); 
 }; 
